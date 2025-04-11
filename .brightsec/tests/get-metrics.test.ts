2fbe850d2fbd75c59eed2b99a7666fda7b875f04
Artem Derevnjuk <artedere@gmail.com>
2025-04-11 21:15:18 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-metrics.test.ts
@@ -0,0 +1,36 @@
+import { test, before, after } from 'node:test';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { SecRunner } from '@sectester/runner';
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+test('GET /metrics', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['excessive_data_exposure', 'csrf', 'improper_asset_management', 'open_database'],
+      attackParamLocations: [AttackParamLocation.HEADER]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/metrics`,
+      headers: {
+        'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'
+      }
+    });
+});
---
+++ .brightsec/tests/put-rest-products-1-reviews.test.ts
@@ -0,0 +1,33 @@
+import { test, before, after } from 'node:test';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { SecRunner } from '@sectester/runner';
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+test('PUT /rest/products/1/reviews', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['nosql', 'csrf', 'xss', 'bopla', 'excessive_data_exposure'],
+      attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.PATH]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.PUT,
+      url: `${baseUrl}/rest/products/1/reviews`
+    });
+});

9f10c9d6b22e874a3738e32755218e1b25a6a5a2
Viachaslau <slava@tssbox.com>
2025-11-29 12:12:24 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-challenges.test.ts
@@ -0,0 +1,58 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/challenges', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'xss'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: "NeuraLegion/juice-shop:master",
+        databases: ["SQLite"],
+        user_roles: {
+          roles: ["customer", "deluxe", "accounting", "admin"]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/Challenges`,
+      body: {
+        id: 1,
+        key: "restfulXssChallenge",
+        name: "Sample Challenge",
+        category: "Security",
+        tags: "xss,security",
+        description: "A challenge to test XSS vulnerabilities.",
+        difficulty: 3,
+        mitigationUrl: "http://example.com/mitigation",
+        solved: false,
+        disabledEnv: null,
+        tutorialOrder: null,
+        codingChallengeStatus: 0,
+        hasCodingChallenge: false
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
